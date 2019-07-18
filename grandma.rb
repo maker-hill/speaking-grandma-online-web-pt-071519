4bc8b97e5cf5f77da80fd98cacc7242a21e057a3
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-18 13:24:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,10 @@ def speak_to_grandma(phrase)
 
   if phrase == "I LOVE YOU GRANDMA" 
     puts "I LOVE YOU TOO PUMPKIN"
+ 
   elsif phrase == String.upcase
    puts "NO, NOT SINCE 1938!"
+ 
   else
   puts 
     "HUH?! SPEAK UP, SONNY!"
